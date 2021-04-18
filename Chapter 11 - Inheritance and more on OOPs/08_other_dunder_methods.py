d174325fa10861a9dd9392d831cbed0f4a283226
Harshit Singh <harman.s2021@gmail.com>
2021-04-18 08:04:11 +0530

    Changes committed

--- Chapter 11 - Inheritance and more on OOPs/08_other_dunder_methods.py
+++ Chapter 11 - Inheritance and more on OOPs/08_other_dunder_methods.py
@@ -1,23 +1,21 @@
-# dunder methods
+
 class Number:
     def __init__(self,num):
         self.num = num
     
     def __add__(self,num2):
         print("Lets add")
-        # return 300
         return self.num + num2.num
 
     def __mul__(self,num2):
         print("Lets multiply")
         return self.num * num2.num
 
-n1 = Number(4)
-n2 = Number(6)
-# n1 + n2
-sum = n1 + n2
-print(sum)
-mul = n1 * n2
-print(mul)
+    def __str__(self):
+        return f"Decimal Number: {self.num}"
 
-# python docs for operator overloading
+    def __len__(self):
+        return 1
+n = Number(9)
+print(n)
+print(len(n))
